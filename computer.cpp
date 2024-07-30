fe465e666417e5743f333427f36a7779778e70c8
tswang <tswang@uwaterloo.ca>
2024-07-30 00:48:37 -0400

    add move command

--- computer.cpp
+++ computer.cpp
@@ -196,7 +196,6 @@ bool Computer::makeMove(ChessBoard& cBoard) {
     string playerColour = isWhite ? "white" : "black";
     string opponentColour = isWhite ? "black" : "white";
 
-<<<<<<< HEAD
     string inputLine; 
 
     while(getline(in, inputLine)) {
@@ -223,27 +222,6 @@ bool Computer::makeMove(ChessBoard& cBoard) {
         } else {
             cerr << "Invalid computer command" <<endl;
         }
-=======
-    if (difficulty == 1) {
-        // random legal moves
-        // idea: find a piece that is on the board and randomly choose a move 
-        return makeMove1(cBoard);
-    } else if (difficulty == 2) {
-        // prefers capturing moves and checks
-
-        // REWRITE WHEN WE HAVE A GOBACK FUNCTION IN CHESSBOARD
-        
-        makeMove2(cBoard);
-    } else if (difficulty == 3) {
-        // prefers avoiding capture, capturing moves, and checks
-        makeMove3(cBoard);         
-
-    } else if(difficulty == 4) {
-        // weighted pieces 
-        return makeMove4(cBoard);
-    } else {
-        cerr << "Invalid computer command" <<endl;
->>>>>>> 5d2c1933e8a549fa82949755241bb0418cc097e1
     }
     
     return true;
